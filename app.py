bf20851de909cee276164e8a4db52549904290d1
Susanna-Ostman <111853950+Susanna-Ostman@users.noreply.github.com>
2022-08-23 14:50:15 +0300

    added more code to app-file

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 #Main application
 
-#Ask user name
+#Ask user name and do they like cats
 
-name = input("What's your name?")
-firstList = []
+name = input("What's your name? ")
+
+likeCats = input("Do you like cats? yes or no ")
+
+if likeCats == "yes":
+    print(name, "yay, you like cats! ")
+
+else:
+    print(name, "oh no! you don't like cats ")
+
+def nameLength(name):
+    """Calculates name length
+
+    Args:
+        name (str): name of the person
+
+    Returns:
+        int: returns length of the name
+    """
+    length = len(name)
+    return length
+
+#Calculate name length
+
+length = nameLength(name)
+print(name, " has ", length, " words.")
